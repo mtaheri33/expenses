c30b2ce220f1d246d886cfaeba0e55a2380160ac
Michael Taheri <michael.taheri33@gmail.com>
2024-10-04 13:45:28 -0700

    updated main.jsx comments

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,8 @@ import {
 import './main.css';
 import Index from './pages/index/Index';
 
-// This creates a router for React to use to handle different pages of
-// the website.
+// This creates the routing between resource paths and React
+// components.
 const router = createBrowserRouter([
   {
     path: '/',
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
 ]);
 
 // This uses the DOM to get the root div element from index.html and
-// then renders the React components within it.
+// then renders the React app within it.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
