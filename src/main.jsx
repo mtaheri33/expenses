8e8788a5e8c963a27aed5750a1ce3c72c90ba0b7
Michael Taheri <michael.taheri33@gmail.com>
2024-10-06 13:35:15 -0700

    imported material ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 // This sets up the React app.
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './main.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import Index from './pages/index/Index';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom';
-import './main.css';
-import Index from './pages/index/Index';
 
 // This creates the routing between resource paths and React
 // components.
@@ -19,9 +24,11 @@ const router = createBrowserRouter([
 ]);
 
 // This uses the DOM to get the root div element from index.html and
-// then renders the React app within it.
+// then renders the React app within it.  CssBaseline removes default
+// browser styles.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <CssBaseline />
     <RouterProvider router={router} />
   </StrictMode>,
 );
