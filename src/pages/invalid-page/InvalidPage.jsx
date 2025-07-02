10db3cbab3786fff37d0db7a09334f37fbc47b4a
Michael Taheri <michael.taheri33@gmail.com>
2025-07-01 19:28:12 -0700

    added link to home for invalid page

--- src/pages/invalid-page/InvalidPage.jsx
+++ src/pages/invalid-page/InvalidPage.jsx
@@ -1,9 +1,11 @@
 // This is the component for any page the user goes to that doesn't exist.
 
+import { Link } from 'react-router';
+
 export default function InvalidPage() {
   return (
     <div className='InvalidPage'>
-      <span>This page does not exist.</span>
+      <span>This page does not exist. <Link to='/'>Home</Link></span>
     </div>
   );
 }
