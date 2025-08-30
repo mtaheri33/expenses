f3547b2cd8e14a4988bbe0d8ceb91d72dc4109b3
Michael Taheri <michael.taheri33@gmail.com>
2025-08-29 20:41:24 -0700

    made user attribute in expenses required, also added index - with the index it is better to get all expenses for a user by querying expenses where user: userId rather than keeping an expenses attribute in the User schema of expense ids

--- api/mongoose/expenses.js
+++ api/mongoose/expenses.js
@@ -10,6 +10,8 @@ const expenseSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
+    required: true,
+    index: true,
   },
 });
 const Expense = mongoose.model('Expense', expenseSchema);
