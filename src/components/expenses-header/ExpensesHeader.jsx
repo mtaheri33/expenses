1c1d310d1f89ca8da0d44f8be2e70f2506725622
Michael Taheri <michael.taheri33@gmail.com>
2026-04-14 20:33:39 -0700

    remove new line with parenthesis since line doesnt go past line length

--- src/components/expenses-header/ExpensesHeader.jsx
+++ src/components/expenses-header/ExpensesHeader.jsx
@@ -10,7 +10,5 @@ export default function ExpensesHeader({ styleClass }) {
     font-size required
   }
   */
-  return (
-    <h1 className={`ExpensesHeader ${styles.ExpensesHeader} ${styleClass}`}>Expenses</h1>
-  );
+  return <h1 className={`ExpensesHeader ${styles.ExpensesHeader} ${styleClass}`}>Expenses</h1>;
 }
