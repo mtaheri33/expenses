5a1c57cfe6029b2bff8f194d61a4971464d3c51a
Michael Taheri <michael.taheri33@gmail.com>
2026-06-10 20:30:37 -0700

    refactored update expense page

--- src/pages/expenses/id/UpdateExpense.jsx
+++ src/pages/expenses/id/UpdateExpense.jsx
@@ -1,37 +1,37 @@
 // This is the component for the /expenses/:expenseId page.
 
+import ExpenseForm from '../../../components/expense-form/ExpenseForm';
 import Navbar from '../../../components/navbar/Navbar';
 import PageLoading from '../../../components/page-loading/PageLoading';
-import UpdateExpenseForm from './components/UpdateExpenseForm';
+import { RequestMethod } from '../../../../constants';
 import InvalidPage from '../../invalid-page/InvalidPage';
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router';
 import { toast } from 'sonner';
+import styles from './UpdateExpense.module.css';
 import { isAuthenticated, getRequest } from '../../../../utilities';
 
 export default function UpdateExpense() {
-  const navigate = useNavigate();
-  const params = useParams();
-  const expenseId = params.expenseId;
   const [isAuthenticatedState, setIsAuthenticatedState] = useState(null);
   const [validExpense, setValidExpense] = useState(null);
   const [expense, setExpense] = useState({});
 
+  const navigate = useNavigate();
+  const params = useParams();
+  const expenseId = params.expenseId;
+
+  async function handleExpenses200Response(response) {
+    const expense = await response.json();
+    setExpense({ ...expense, amount: String(expense.amount) });
+    setValidExpense(true);
+  }
+  function handleExpenses404Response() {
+    setValidExpense(false);
+  }
   function handleDefaultResponse() {
     toast.error('Sorry, an error occurred. Please try again later.');
     setValidExpense(null);
   }
-
-  function handleExpenses404Response() {
-    setValidExpense(false);
-  }
-
-  async function handleExpenses200Response(response) {
-    setValidExpense(true);
-    const expense = await response.json();
-    setExpense(expense);
-  }
-
   async function handleExpensesResponse(response) {
     switch (response.status) {
       case 200:
@@ -44,13 +44,8 @@ export default function UpdateExpense() {
         handleDefaultResponse();
     }
   }
-
   async function getExpense() {
-    const expensesResponse = await getRequest(
-      `/api/expenses/${expenseId}`,
-      undefined,
-      true,
-    );
+    const expensesResponse = await getRequest(`/api/expenses/${expenseId}`, undefined, true);
     await handleExpensesResponse(expensesResponse);
   }
 
@@ -58,7 +53,6 @@ export default function UpdateExpense() {
     await getExpense();
     setIsAuthenticatedState(true);
   }
-
   async function checkIsAuthenticated() {
     const authenticated = await isAuthenticated();
     if (authenticated) {
@@ -87,8 +81,13 @@ export default function UpdateExpense() {
   return (
     <div className='UpdateExpense'>
       <Navbar />
-      <main>
-        <UpdateExpenseForm expense={expense} />
+      <main className={styles.main}>
+        <h1 className={styles.h1}>Edit Expense</h1>
+        <ExpenseForm
+          expenseData={expense}
+          requestMethod={RequestMethod.PATCH}
+          requestUrl={`/api/expenses/${expenseId}`}
+        />
       </main>
     </div>
   );
